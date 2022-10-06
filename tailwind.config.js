26cdf5264207d86d60e371eab90ecd09f59347ec
yongggnd <yongkerkwith@gmail.com>
2022-10-06 16:20:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        primary: "#FF8D00",
+        secondary: "#37A03A",
+        background: "#F9F9F9",
+        "background-orange": "#FFFCF9",
+        white: "#FFFFFF",
+        red: "#F26161",
+        gray: "#FBFBFF",
+        "gray-header": "#F7F8FA",
+        black: "#4E4E4E",
+        green: "#ECFFEC",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        kanit: ["Kanit", "sans-serif"],
+        noto: ["Noto Sans Thai", "serif"],
+      },
+      fontWeight: {
+        title: "600",
+      },
+      fontSize: {
+        title: "18px",
+      },
+    },
+  },
+  plugins: [],
+};
