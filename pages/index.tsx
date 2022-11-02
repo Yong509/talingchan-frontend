bdc37d1d3acf943ef0ad9a503da2333bb35f2270
yongggnd <yongkerkwith@gmail.com>
2022-11-02 17:17:28 +0700

    push quantity and unit revision

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,29 +49,29 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       console.log(error);
     });
 
-  // for (let index = 0; index < data.length; index++) {
-  //   let total: number = 0;
-  //   for (let j = 0; j < lotProduct.length; j++) {
-  //     if (data[index].PID == lotProduct[j].PID) {
-  //       total += lotProduct[j].LotQty;
-  //     }
-  //   }
-  //   data[index].PQuantity = total;
-  // }
+  for (let index = 0; index < data.length; index++) {
+    let total: number = 0;
+    for (let j = 0; j < lotProduct.length; j++) {
+      if (data[index].PID == lotProduct[j].PID) {
+        total += lotProduct[j].LotQty;
+      }
+    }
+    data[index].PQuantity = total;
+  }
 
-  // for (let index = 0; index < data.length; index++) {
-  //   let tempUID: number = 0;
-  //   for (let j = 0; j < lotProduct.length; j++) {
-  //     if (data[index].PID == lotProduct[j].PID) {
-  //       tempUID = lotProduct[j].UID;
-  //       for (let k = 0; k < unitProduct.length; k++) {
-  //         if (unitProduct[k].UID == tempUID) {
-  //           data[index].PUnit = unitProduct[k].UDetail;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  for (let index = 0; index < data.length; index++) {
+    let tempUID: number = 0;
+    for (let j = 0; j < lotProduct.length; j++) {
+      if (data[index].PID == lotProduct[j].PID) {
+        tempUID = lotProduct[j].UID;
+        for (let k = 0; k < unitProduct.length; k++) {
+          if (unitProduct[k].UID == tempUID) {
+            data[index].PUnit = unitProduct[k].UDetail;
+          }
+        }
+      }
+    }
+  }
   return {
     props: {
       data,
