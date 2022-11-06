d74312c5a10dfc699b600786c70af162fdb3075b
yongggnd <yongkerkwith@gmail.com>
2022-11-07 02:08:10 +0700

    testing s3 upload

---
+++ s3config.ts
@@ -0,0 +1,6 @@
+export const s3Config = {
+  bucketName: "talingchan-storage-v1",
+  region: "ap-southeast-1",
+  accessKeyId: "AKIA2FTCLXV5T5FVVNFH",
+  secretAccessKey: "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",
+};
