fda7e1dc455d915e20f56058ca2e69749ea92e32
Yong Supawattanakul <52814140+Yong509@users.noreply.github.com>
2023-01-15 14:45:26 +0700

    Delete s3config.ts

--- s3config.ts
+++
@@ -1,6 +0,0 @@
-export const s3Config = {
-  bucketName: "talingchan-storage-v1",
-  region: "ap-southeast-1",
-  accessKeyId: "AKIA2FTCLXV5T5FVVNFH",
-  secretAccessKey: "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",
-};
